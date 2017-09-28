bb78ec35845f89f8a64050886b1bc4115e0829ee
carlosferreirap <32198688+carlosferreirap@users.noreply.github.com>
2017-09-28 16:00:34 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ dict0 = {}
 dict1 = {}
 
 
-def scanToDict(listPath, dict):
+def scanToDict(path, listPath, dict):
     """
     Complète le dictionnaire avec le path de l'objet analysé en key et size, time en data
     """
     for b in listPath:
-        key = os.path.join(b)
+        key = os.path.join(path, b)
         size = os.path.getsize(key)
         time = os.path.getmtime(key)
         dict[key] = [size, time]  # path = "G:\Cours ISEN"
@@ -25,14 +25,14 @@ def scanDoctor(path,nbrSousDossier = 0):
 
     fichier = []
     if nbrSousDossier == 0 :
-        l = glob.glob(path + '\\*') #glob.glob vas lister le contenu d'un repertoire
+        l = os.listdir(path) #glob.glob vas lister le contenu d'un repertoire
         for i in l:
             fichier.append(i)
         return fichier
 
     else :
         current =0
-        aReturn = listdirectory(path,nbrSousDossier-1,current) #-1 a cause d'un coup d'ajustement
+        aReturn = maListDirectory(path,nbrSousDossier-1,current) #-1 a cause d'un coup d'ajustement
         return aReturn
 
 def listdirectory(path,max,valeurActuelle):
@@ -47,10 +47,29 @@ def listdirectory(path,max,valeurActuelle):
         else:
             fichier.append(i)
 
-
     return (fichier)
 
-def scanLaunch(path="G:\Cours ISEN", nbSousDossier = 0):
+def maListDirectory(path, max, valeurActuelle):
+    list = []
+    l = os.listdir(path)
+    for i in l:
+        if os.path.isdir(i) and (valeurActuelle <= max) : #Valeur actuelle Permet de delimiter le nombre de recurrence de la recusivité
+            list.extend(maListDirectory(i,max,valeurActuelle+1))
+
+def scanComparison(dict0, dict1):
+    for a in dict1:
+        if a in dict0:
+            checkSizeTime(dict0[a], dict1[a])
+        else:
+            print("Do nothing")
+
+def checkSizeTime(a, b):
+    if (a[0] == b[0]) & (a[1] == b[1]):
+        return False
+    else:
+        return True
+
+def scanLaunch(path, nbSousDossier = 0):
     """Fonction principale du scan
     Vérifie si il s'agit du premier scan ou si on a déjà scanné avant"""
     listFiles= []
@@ -59,40 +78,25 @@ def scanLaunch(path="G:\Cours ISEN", nbSousDossier = 0):
         # Premier scan
         print("Vide !")
         listFiles = scanDoctor(path, nbrSousDossier=0)
-        scanToDict(listFiles, dict0)
+        scanToDict(path, listFiles, dict0)
     else:
         # On a déjà un scan
         print("Second scan")
         listFiles = scanDoctor(path, nbrSousDossier=0)
-        scanToDict(listFiles, dict1)
+        scanToDict(path, listFiles, dict1)
      # à ajouter la fonction de comparaison (dans le else)
 
 
-def writeScanToFile(path="G:\Cours ISEN"):
-    dirs = os.listdir(path)
-    # Open and Write on file "PATH  SIZE  TIME"
-    fo = open("foo.txt", "w")
-    for b in dirs:
-        c = os.path.join(path, b)
-        fo.write(c)
-        fo.write('\t')
-        fo.write('%d' % os.path.getsize(c))
-        fo.write('\t')
-        fo.write('%d' % os.path.getmtime(c))
-        fo.write('\n')
-    fo.close()
-
-
 def mymain():
-    scanLaunch("G:\Cours ISEN")
+    scanLaunch("C:\\Users\André\PycharmProjects\TP1", 2)
     for a in dict0:
         print(a, dict0[a])
     from time import sleep
-    sleep(3)
-    scanLaunch("G:\Cours ISEN")
+    sleep(1)
+    scanLaunch("C:\\Users\André\PycharmProjects\TP1", 2)
     for a in dict1:
         print(a, dict1[a])
-
+    scanComparison(dict0,dict1)
 
 if __name__ == "__main__":
     mymain()
